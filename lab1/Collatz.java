03e28584430a0b3dc6c1be671d388c8051089397
yysfg <10479189+yysfg@user.noreply.gitee.com>
2023-04-25 12:42:51 +0800

    done with Collatz

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -5,14 +5,13 @@ public class Collatz {
 
     /** Buggy implementation of nextNumber! */
     public static int nextNumber(int n) {
-        if (n  == 128) {
-            return 1;
-        } else if (n == 5) {
-            return 3 * n + 1;
+        if ((n & 1) == 0) {
+            return n >> 1;
         } else {
-            return n * 2;
+            return n * 3 + 1;
         }
     }
+
 
     public static void main(String[] args) {
         int n = 5;
